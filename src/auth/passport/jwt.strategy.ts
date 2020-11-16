127c4d97ee5d9770bc8644b59f8c6ec2842d22d6
serjo96 <king-beer@yandex.ru>
2020-11-17 01:52:11 +0500

    Added jwt strategy secret key form config service

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -5,20 +5,25 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserDto } from '@user/dto/user.dto';
 
 import { AuthService } from '../auth.service';
+import { ConfigService } from '../../config/config.service';
 
 import { JwtPayload } from './jwt.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.SECRETKEY,
+      secretOrKey: configService.jwtConfig.secretCode,
+      ignoreExpiration: false,
     });
   }
 
   async validate(payload: JwtPayload): Promise<UserDto> {
-    const user = await this.authService.validateUser(payload);
+    const user = await this.authService.validateUserToken(payload);
     if (!user) {
       throw new UnauthorizedException('Invalid token');
     }
