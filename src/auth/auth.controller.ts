333bb91c0662aac4f21a02484817401916ffeb00
serjo96 <king-beer@yandex.ru>
2020-11-17 01:58:20 +0500

    Added guard admin for register router; Added login methods;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { CreateUserDto } from '@user/dto/create-user.dto';
 import { UserDto } from '@user/dto/user.dto';
+import { UsersService } from '@user/users.service';
 
 import { BadRequestException } from '../common/exceptions/bad-request';
+import { Roles } from '../common/decorators/roles';
 
 import { AuthService } from './auth.service';
 import { LoginStatus } from './interfaces/login-status.interface';
@@ -11,9 +14,13 @@ import { LoginByEmail } from './dto/login.dto';
 
 @Controller('/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post('/register')
+  @Roles('admin')
   @HttpCode(200)
   public async register(
     @Body() createUserDto: CreateUserDto,
@@ -27,10 +34,10 @@ export class AuthController {
     }
   }
 
+  @UseGuards(AuthGuard('local'))
   @Post('login')
   @HttpCode(200)
-  public async login(@Body() loginUserDto: LoginByEmail): Promise<LoginStatus> {
-    return await this.authService.login(loginUserDto);
+  public async login(@Request() req, @Body() login: LoginByEmail) {
+    return await this.authService.login(req.user);
   }
-
 }
