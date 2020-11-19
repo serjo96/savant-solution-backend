656c4a2a46391eb7047e0be46aadc9c2117dbaf5
serjo96 <king-beer@yandex.ru>
2020-11-19 19:09:49 +0500

    Added user dto

---
+++ src/auth/dto/user.dto.ts
@@ -0,0 +1,18 @@
+import { RolesEnum, User } from '@user/users.entity';
+
+export class UserResponseDto {
+  constructor(object: User) {
+    this.id = object.id;
+    this.email = object.email;
+    this.roles = RolesEnum[object.roles];
+    if ( object.profile ) {
+      this.name = object.profile.name;
+      this.photoURL = object.profile.photoURL;
+    }
+  }
+  readonly id: string;
+  readonly name: string;
+  readonly email: string;
+  readonly photoURL: string;
+  readonly roles: string;
+}
