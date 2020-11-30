72d3e7a359342213b8a0af1170f1037ab94fc7b2
serjo96 <king-beer@yandex.ru>
2020-12-01 01:53:16 +0500

    Added interface for item;

---
+++ src/items/interfaces/item.interface.ts
@@ -0,0 +1,10 @@
+export default interface ItemInterface {
+  id: string;
+  quantity: number;
+  itemNumber: string;
+  amazonSku: string;
+  status: string;
+  threshold: number;
+  supplier: string;
+  altSupplier: string;
+}
