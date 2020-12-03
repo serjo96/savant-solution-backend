02b3b027eed6e21ea6155f956b237708c1437208
serjo96 <king-beer@yandex.ru>
2020-12-04 03:20:03 +0500

    Added items service;

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { res } from 'pino-std-serializers';
+import { Repository } from 'typeorm';
+
+import { ItemDto } from './dto/item.dto';
+import { Items } from './item.entity';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Items)
+    private readonly productsRepository: Repository<Items>,
+  ) {}
+
+  async find(where: any): Promise<Items[]> {
+    return await this.productsRepository.find(where);
+  }
+
+  async findOne(where: any): Promise<Items> {
+    return await this.productsRepository.findOne(where)
+  }
+
+  async getAll(where?: any): Promise<Items[]> {
+    const clause: any = {
+      where,
+      order: {
+        createdAt: 'ASC',
+      },
+    };
+
+    return await this.productsRepository.find(clause);
+  }
+
+  async delete(where: any): Promise<any> {
+    return await this.productsRepository.softDelete(where);
+  }
+
+  async save(data: ItemDto): Promise<Items> {
+    let entity = data;
+    if (!(data instanceof Items)) {
+      entity = Items.create(data);
+    }
+    return await this.productsRepository.save(entity);
+  }
+
+  async updateRaw({ where, data }: { where: any; data: any }): Promise<any> {
+    return await this.productsRepository.update(where, data);
+  }
+
+}
