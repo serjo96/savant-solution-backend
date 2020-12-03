070164f0aca8c6b37596b025bfb1f0cf7c9ecd01
serjo96 <king-beer@yandex.ru>
2020-12-04 03:11:06 +0500

    Added transform interceptor

---
+++ src/common/interceptors/TransformInterceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+type ClassType<T> = new() => T;
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<Partial<T>, T> {
+
+  constructor(private readonly classType: ClassType<T>) {}
+
+  intercept(context: ExecutionContext, next: CallHandler<T> ): Observable<T> {
+    return next.handle().pipe(map(data => plainToClass(this.classType, data)));
+  }
+}
