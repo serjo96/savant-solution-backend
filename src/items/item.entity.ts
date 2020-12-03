42dff8de2c666ded69a07926ccbc96a5b2826425
serjo96 <king-beer@yandex.ru>
2020-12-04 03:18:57 +0500

    Add item entity

---
+++ src/items/item.entity.ts
@@ -0,0 +1,63 @@
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { BaseEntity } from '../common/base-entity';
+
+export enum StatusEnum {
+  ACTIVE = 1,
+  INACTIVE = 0,
+}
+
+@Entity('items')
+export class Items extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  itemNumber: string;
+
+  @Column({
+    type: 'integer',
+    nullable: false,
+  })
+  quantity: number;
+
+  @Column({
+    type: 'integer',
+    nullable: true,
+  })
+  threshold?: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  amazonSku: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  supplier: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  altSupplier?: string;
+
+  @Column({
+    type: 'enum',
+    enum: StatusEnum,
+    default: StatusEnum.ACTIVE,
+  })
+  public status: StatusEnum;
+
+}
