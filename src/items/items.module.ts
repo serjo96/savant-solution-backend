49117300bb34dcdacca887b8398f96d89a247b84
serjo96 <king-beer@yandex.ru>
2020-12-04 03:19:49 +0500

    Added items module;

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Items } from './item.entity';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Items]),
+  ],
   controllers: [ItemsController],
   providers: [ItemsService],
+  exports: [ItemsService],
 })
 export class ItemsModule {}
