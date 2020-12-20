fed4b17d88ffd44f25f8ae97b5304f94f016aa34
serjo96 <king-beer@yandex.ru>
2020-12-21 01:13:06 +0500

    Added UserWithToken type for response reg endpoint;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,17 @@
 import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 import { CreateUserDto } from '@user/dto/create-user.dto';
-import { UserDto } from '@user/dto/user.dto';
-import { UsersService } from '@user/users.service';
 
 import { BadRequestException } from '../common/exceptions/bad-request';
-import { Roles } from '../common/decorators/roles';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { Roles } from '../common/decorators/roles';
 import { RolesGuard } from '../common/guards/roles.guard';
 
 import { AuthService } from './auth.service';
-import { LoginStatus } from './interfaces/login-status.interface';
 import { LoginByEmail } from './dto/login.dto';
+import { UserWithToken } from './interfaces/user-with-token.interface';
 
 @Controller('/auth')
 export class AuthController {
@@ -28,7 +26,7 @@ export class AuthController {
   public async register(
     @Body() createUserDto: CreateUserDto,
     @Req() req: Request
-  ): Promise<LoginStatus> {
+  ): Promise<UserWithToken> {
     try {
       return await this.authService.register(createUserDto);
     } catch (error) {
