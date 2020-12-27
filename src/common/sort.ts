eacd9c2a6f87455a5b207230350a242dcd4cc2db
serjo96 <king-beer@yandex.ru>
2020-12-27 23:21:25 +0500

    Created sortBy helper with validation dto;

---
+++ src/common/sort.ts
@@ -0,0 +1,40 @@
+import { IsOptional } from 'class-validator';
+import { IsOrderBy, IsOrderDirection } from './decorators/sortValidation';
+import { PaginatorQuery } from './paginator';
+
+export class SortBy {
+  id = '';
+  createdAt = '';
+  updatedAt = '';
+  deletedAt = '';
+}
+
+export class SortWithPaginationQuery extends PaginatorQuery {
+  @IsOptional()
+  @IsOrderDirection()
+  sort_by: string;
+}
+
+export default interface ISort {
+  order: {
+    [key: string]: string;
+  };
+}
+
+export const sort = (query: SortWithPaginationQuery): ISort => {
+  const result: ISort = {
+    order: {
+      createdAt: 'asc',
+    },
+  };
+
+  const { sort_by } = query;
+
+  if (sort_by) {
+    const [sortType, sortDir] = sort_by.split('.');
+    result.order = {};
+    result.order[sortType] = sortDir.toUpperCase();
+  }
+
+  return result;
+};
