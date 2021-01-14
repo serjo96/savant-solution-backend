d8fc6d824b285d2de3606aaa2ff0c564dd1e785b
serjo96 <king-beer@yandex.ru>
2021-01-15 01:57:54 +0500

    Added user settings entity;

--- src/users/user-settings.entity.ts
+++ src/users/user-settings.entity.ts
@@ -1,22 +1,32 @@
-import * as bcrypt from 'bcrypt';
 import {
   BeforeInsert,
-  BeforeUpdate,
   Column,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
 import { BaseEntity } from '../common/base-entity';
-
-
+import { ItemsColumnsVisibilityEntity } from '../items/items-columns-visibility.entity';
+import { OrdersColumnsVisibilityEntity } from '../orders/orders-columns-visibility.entity';
 
 @Entity('user-settings')
 export class UserSettings extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @OneToOne(() => OrdersColumnsVisibilityEntity, { eager: true, cascade: true })
+  @JoinColumn()
+  ordersTable: OrdersColumnsVisibilityEntity;
 
+  @OneToOne(() => ItemsColumnsVisibilityEntity, { eager: true, cascade: true })
+  @JoinColumn()
+  itemsTable: ItemsColumnsVisibilityEntity;
 
-
+  @BeforeInsert()
+  createInitData() {
+    this.ordersTable = new OrdersColumnsVisibilityEntity();
+    this.itemsTable = new ItemsColumnsVisibilityEntity();
+  }
 }
