461b7a2d5d2a9459da8acea43371f37dc961d6f1
serjo96 <king-beer@yandex.ru>
2021-01-15 03:21:07 +0500

    Added orders tests;

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { OrdersMock } from './orders.mock';
 import { OrdersService } from './orders.service';
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  const OrdersServiceProvider = {
+    provide: OrdersService,
+    useClass: OrdersMock,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [OrdersServiceProvider],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
