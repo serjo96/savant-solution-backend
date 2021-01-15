5ab2b5ee01b90b4a4be46b52feaff49fb9fccde3
serjo96 <king-beer@yandex.ru>
2021-01-16 03:55:27 +0500

    Copy all project to prod;

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   apps : [{
     name: "kryber-backend",
-    script: "./src/main.js",
+    script: "./dist/main.js",
     restart_delay: 3000,
-    watch: ["./src"],
+    watch: ["./dist"],
     // Delay between restart
     watch_delay: 1000,
-    env: {
-      NODE_ENV: "production",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
   }]
 };
