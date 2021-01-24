001a72ab78158dd938b993262b4a44e92a30c1c5
serjo96 <king-beer@yandex.ru>
2021-01-24 23:56:48 +0500

    ADded conditions for date migration;

--- migrations/1611514200672-addOrderDateColumn.ts
+++ migrations/1611514200672-addOrderDateColumn.ts
@@ -3,10 +3,10 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class addOrderDateColumn1611514200672 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" ADD COLUMN "orderDate" timestamptz default NULL`);
+        await queryRunner.query(`ALTER TABLE "orders" ADD COLUMN IF NOT EXISTS "orderDate" timestamptz default NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "orderDate"`);
+        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN IF EXISTS "orderDate"`);
     }
 }
