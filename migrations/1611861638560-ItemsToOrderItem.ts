d1c3572815ffb1dbaad8a6b89e64eb24b26adf81
Maksim Zakharov <max89701@gmail.com>
2021-01-28 22:30:44 +0300

    fix

--- migrations/1611861638560-ItemsToOrderItem.ts
+++ migrations/1611861638560-ItemsToOrderItem.ts
@@ -5,15 +5,15 @@ export class ItemsToOrderItem1611861638560 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "items" DROP CONSTRAINT IF EXISTS "FK_9e039229fb4b5a379ab79e887ad"`);
-        await queryRunner.query(`CREATE TYPE "order-items_graingershipmethod_enum" AS ENUM('1', '2')`);
-        await queryRunner.query(`CREATE TABLE "order-items" ("id" uuid NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT now(), "deletedAt" TIMESTAMP WITH TIME ZONE, "amazonItemId" character varying NOT NULL, "amazonSku" character varying NOT NULL, "amazonQuantity" integer NOT NULL DEFAULT '0', "graingerTrackingNumber" character varying, "graingerShipMethod" "order-items_graingershipmethod_enum", "graingerOrderId" character varying, "graingerShipDate" date, "graingerWebNumber" character varying, "note" character varying, "itemId" uuid, "orderId" uuid, CONSTRAINT "UQ_a71e258360def237e6a2e1907d1" UNIQUE ("amazonItemId"), CONSTRAINT "PK_605fbaee38242facaa1a34b67ad" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE IF NOT EXISTS "order-items_graingershipmethod_enum" AS ENUM('1', '2')`);
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "order-items" ("id" uuid NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT now(), "deletedAt" TIMESTAMP WITH TIME ZONE, "amazonItemId" character varying NOT NULL, "amazonSku" character varying NOT NULL, "amazonQuantity" integer NOT NULL DEFAULT '0', "graingerTrackingNumber" character varying, "graingerShipMethod" "order-items_graingershipmethod_enum", "graingerOrderId" character varying, "graingerShipDate" date, "graingerWebNumber" character varying, "note" character varying, "itemId" uuid, "orderId" uuid, CONSTRAINT "UQ_a71e258360def237e6a2e1907d1" UNIQUE ("amazonItemId"), CONSTRAINT "PK_605fbaee38242facaa1a34b67ad" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "note"`);
         await queryRunner.query(`ALTER TABLE "items" DROP CONSTRAINT IF EXISTS "UQ_5a7e9916adb45b3032a21c85877"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "amazonItemId"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "amazonQuantity"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "graingerTrackingNumber"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "graingerShipMethod"`);
-        await queryRunner.query(`DROP TYPE "public"."items_graingershipmethod_enum"`);
+        await queryRunner.query(`DROP TYPE IF EXISTS "public"."items_graingershipmethod_enum"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "graingerOrderId"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "graingerShipDate"`);
         await queryRunner.query(`ALTER TABLE "items" DROP COLUMN IF EXISTS "graingerWebNumber"`);
@@ -33,15 +33,15 @@ export class ItemsToOrderItem1611861638560 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "items" ADD "graingerWebNumber" character varying`);
         await queryRunner.query(`ALTER TABLE "items" ADD "graingerShipDate" date`);
         await queryRunner.query(`ALTER TABLE "items" ADD "graingerOrderId" character varying`);
-        await queryRunner.query(`CREATE TYPE "public"."items_graingershipmethod_enum" AS ENUM('1', '2')`);
+        await queryRunner.query(`CREATE TYPE IF NOT EXISTS "public"."items_graingershipmethod_enum" AS ENUM('1', '2')`);
         await queryRunner.query(`ALTER TABLE "items" ADD "graingerShipMethod" "items_graingershipmethod_enum"`);
         await queryRunner.query(`ALTER TABLE "items" ADD "graingerTrackingNumber" character varying`);
         await queryRunner.query(`ALTER TABLE "items" ADD "amazonQuantity" integer NOT NULL DEFAULT '0'`);
         await queryRunner.query(`ALTER TABLE "items" ADD "amazonItemId" character varying NOT NULL`);
         await queryRunner.query(`ALTER TABLE "items" ADD CONSTRAINT "UQ_5a7e9916adb45b3032a21c85877" UNIQUE ("amazonItemId")`);
         await queryRunner.query(`ALTER TABLE "items" ADD "note" character varying`);
-        await queryRunner.query(`DROP TABLE "order-items"`);
-        await queryRunner.query(`DROP TYPE "order-items_graingershipmethod_enum"`);
+        await queryRunner.query(`DROP TABLE IF EXISTS "order-items"`);
+        await queryRunner.query(`DROP TYPE IF EXISTS "order-items_graingershipmethod_enum"`);
         await queryRunner.query(`ALTER TABLE "items" ADD CONSTRAINT "FK_9e039229fb4b5a379ab79e887ad" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
