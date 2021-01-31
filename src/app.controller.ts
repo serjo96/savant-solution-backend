dac97e4177fa4217eac728e49bb1d5ecdaf72865
Maksim Zakharov <max89701@gmail.com>
2021-01-31 17:45:11 +0300

    тригер cicd по версии

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { fsReadFile } from 'ts-loader/dist/utils';
 
 @Controller()
 export class AppController {
 
   @Get()
   healthCheck() {
-    return 'WORKED!';
+    return `Version: ${JSON.parse(fsReadFile('package.json')).version}`;
   }
 }
