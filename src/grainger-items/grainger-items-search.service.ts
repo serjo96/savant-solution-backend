4b10c6bf1514d8a49860bd65f6a6e1c84283383b
serjo96 <king-beer@yandex.ru>
2021-02-01 20:49:28 +0500

    Committed uploading array of items for elastic;

--- src/grainger-items/grainger-items-search.service.ts
+++ src/grainger-items/grainger-items-search.service.ts
@@ -28,15 +28,15 @@ export class GraingerItemsSearchService {
   }
 
   save<GraingerItem>(data: GraingerItem | Array<GraingerItem>): any {
-    let response;
+    // let response;
     // if (Array.isArray(data)) {
     //   data.forEach((gItem: GraingerItem) => {
     //     response = this.searchService.createIndex(gItem, this.elasticIndex);
     //   });
     // } else {
-    response = this.searchService.createIndex(data, this.elasticIndex);
+    // response = this.searchService.createIndex(data, this.elasticIndex);
     // }
-    return response;
+    return this.searchService.createIndex(data, this.elasticIndex);
   }
 
   update(data: Partial<GraingerItem>) {
