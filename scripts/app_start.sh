dea7b42fd1d091a4fbe8d4c4f638636b16f211bb
serjo96 <king-beer@yandex.ru>
2021-02-05 13:40:33 +0500

    Try new fix for start script;

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/etc
-cp ./production.env ../krayber
+yes | cp ./production.env ../krayber
 cd /home/ubuntu/krayber
 echo START PROD
 pm2 startOrRestart ecosystem.config.js
