d3026486c5022e004f3490459f89c0c22ba6723e
serjo96 <king-beer@yandex.ru>
2021-02-05 13:55:55 +0500

    Added start restart only for specific name;

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/etc
 yes | cp ./production.env ../krayber
 cd /home/ubuntu/krayber
 echo START PROD
-pm2 startOrRestart ecosystem.config.js
+pm2 startOrRestart ecosystem.config.js --only kryber-backend
