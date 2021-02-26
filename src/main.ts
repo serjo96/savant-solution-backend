295692f32dd657ee3e8b6e3e419b7241a8909eca
Maksim Zakharov <max89701@gmail.com>
2021-02-27 00:14:29 +0300

    добавим обработчик 500 ошибок с стектрейсом

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ErrorFilter } from './common/filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(), new ErrorFilter());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
