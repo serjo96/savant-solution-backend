c75765c785f9ec4a3ff9d9930a4916a1e7006f26
serjo96 <king-beer@yandex.ru>
2021-02-27 13:01:10 +0500

    Removed filter exeption

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { ErrorFilter } from './common/filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter(), new ErrorFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
