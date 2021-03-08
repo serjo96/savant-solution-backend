1f89990f14de06bc20fa495248e3ea4c17664d74
Maksim Zakharov <max89701@gmail.com>
2021-03-08 19:06:38 +0300

    stack

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { SentryService } from '@ntegral/nestjs-sentry';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ErrorFilter } from './common/filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new ErrorFilter(), new HttpExceptionFilter());
   app.useLogger(app.get(SentryService));
   await app.listen(process.env.PORT || 3000);
 }
